1d243b6c2435d8f527aaa6189b1318b8ad6668e2
Maxim Kuznetsov <kyzya96@gmail.com>
2021-06-23 09:53:44 +0300

    update testrail reporter

--- cypress/integration/simple_tests/sample_spec.js
+++ cypress/integration/simple_tests/sample_spec.js
@@ -1,9 +1,3 @@
-// describe('My First Test', () => {
-//   it('ADD HERE Cxxx (IDs of test cases) split by space. Does not do much!', () => {
-//     expect(true).to.equal(true)
-//   })
-// })
-
 describe('My First Test', () => {
   it('ADD HERE C4949 (IDs of test cases) split by space.', () => {
     expect(true).to.equal(true)
