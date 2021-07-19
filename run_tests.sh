2f4ec69bcf1437ec32ef8359b3d1a13491caf17c
Maxim Kuznetsov <kyzya96@gmail.com>
2021-07-19 12:26:02 +0300

    run_tests.sh update

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+# The script reading file line by line splits each line by ":" and sets them as environment variables, then runs cypress with new params.
+# Used in Github Actions and report to TestRail
+# Can be run locally
+exit_code=0
 while IFS= read -r line; do
    testrailId=$(echo $line | awk '{split($0, a, ":"); print a[1]}')
    spec=$(echo $line | awk '{split($0, a, ":"); print a[2]}')
@@ -7,5 +10,6 @@ while IFS= read -r line; do
    export CYPRESS_SPEC="${spec}"
 
    npx cypress run --spec $CYPRESS_SPEC --env $CYPRESS_ENV
-
+   exit_code=$((exit_code + $(echo $?)))
 done < test_cases.txt
+exit $exit_code
